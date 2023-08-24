833e331faf65521f2f9c1efc3a60cc416576f826
hilyas <hilyas@users.noreply.github.com>
2023-08-24 08:36:41 +0100

    support for either url or file path

--- cli.py
+++ cli.py
@@ -2,17 +2,25 @@ import click
 from text_analyzer import TextStatistics, TextNLP
 from text_scraper import TextScraper, WebRequester
 
+def read_content_from_file(file_path: str) -> str:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
 @click.group()
 def main():
     """Advanced CLI for textual analysis of scraped content."""
     pass
 
 @main.command()
+@click.argument(
+    "source", 
+    required=True, 
+    type=str) 
 @click.option(
-    "--url",
-    prompt=True,
-    help="URL to analyze."
-)
+    "--source-type", 
+    type=click.Choice(["url", "file"]), 
+    default="url", 
+    help="Specify if the source is a URL or file path.")
 @click.option(
     "--top",
     multiple=True,
@@ -31,13 +39,16 @@ def main():
     is_flag=True,
     help="Verbose mode: print more data."
 )
-def analyze(url, top, type, verbose):
-    requester = WebRequester()
-    scraper = TextScraper(requester)
-    content = scraper.scrape_website(url=url)
+def analyze(top, source, source_type, type, verbose):
+    if source_type == "url":
+        requester = WebRequester()
+        scraper = TextScraper(requester)
+        content = scraper.scrape_website(url=source)
+    else:
+        content = read_content_from_file(source)
     
     if not content:
-        click.echo(f"Failed to scrape content from {url}.")
+        click.echo(f"Failed to retrieve content from {source}.")
         return
     
     text_statistics = TextStatistics(content=content)
@@ -80,7 +91,7 @@ def analyze(url, top, type, verbose):
 
     if verbose:
         click.echo("\nVerbose Mode:")
-        click.echo(f"URL analyzed: {url}")
+        click.echo(f"Source analyzed: {source}")
         click.echo(f"Total unique words: {len(word_freq)}")
         click.echo(f"Total unique letters: {len(letter_freq)}")
 
