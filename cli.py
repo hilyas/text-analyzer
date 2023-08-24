f1c877d54771252d8ee310500679f741de362dfd
hilyas <hilyas@users.noreply.github.com>
2023-08-24 09:18:52 +0100

    break up the analyze function

--- cli.py
+++ cli.py
@@ -5,6 +5,43 @@ from text_scraper import TextScraper, WebRequester
 def read_content_from_file(file_path: str) -> str:
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
+    
+def get_content_from_source(source, source_type):
+    """Retrieve content based on the source type (URL or file)"""
+    if source_type == "url":
+        requester = WebRequester()
+        scraper = TextScraper(requester)
+        return scraper.scrape_website(url=source)
+    else:  # source_type == "file"
+        return read_content_from_file(source)
+
+def perform_text_analysis(content):
+    """Analyze text and return the stats"""
+    text_statistics = TextStatistics(content=content)
+    return text_statistics.analyze_text()
+
+def perform_sentiment_analysis(content):
+    """Perform sentiment analysis and return the sentiment"""
+    text_nlp = TextNLP(content=content)
+    return text_nlp.get_sentiment()
+
+def display_text_analysis(total, word_freq, avg_word_length, letter_freq, total_sentences, type, top):
+    """Display the text analysis results"""
+    click.echo(f"Total words: {total}")
+    click.echo(f"Average word length: {avg_word_length:.2f}")
+    click.echo(f"Total sentences: {total_sentences}")
+
+    if type in ["words", "both"]:
+        for top_count in top:
+            click.echo(f"\nTop {top_count} frequent words:")
+            for word, freq in word_freq.most_common(top_count):
+                click.echo(f"{word}: {freq}")
+
+    if type in ["letters", "both"]:
+        for top_count in top:
+            click.echo(f"\nTop {top_count} frequent letters:")
+            for letter, freq in letter_freq.most_common(top_count):
+                click.echo(f"{letter}: {freq}")
 
 @click.group()
 def main():
@@ -40,54 +77,30 @@ def main():
     help="Verbose mode: print more data."
 )
 def analyze(top, source, source_type, type, verbose):
-    if source_type == "url":
-        requester = WebRequester()
-        scraper = TextScraper(requester)
-        content = scraper.scrape_website(url=source)
-    else:
-        content = read_content_from_file(source)
-    
+    """Main analyze method that orchestrates the various functions"""
+    content = get_content_from_source(source, source_type)
+
     if not content:
         click.echo(f"Failed to retrieve content from {source}.")
         return
-    
-    text_statistics = TextStatistics(content=content)
-    
+
     try:
-        total, word_freq, avg_word_length, letter_freq, total_sentences = text_statistics.analyze_text()
+        total, word_freq, avg_word_length, letter_freq, total_sentences = perform_text_analysis(content)
+        display_text_analysis(total, word_freq, avg_word_length, letter_freq, total_sentences, type, top)
     except Exception as e:
         click.echo(f"An error occurred during analysis: {e}")
         return
-    
-    click.echo(f"Total words: {total}")
-    click.echo(f"Average word length: {avg_word_length:.2f}")
-    click.echo(f"Total sentences: {total_sentences}")
-    
-    text_nlp = TextNLP(content=content)
-    
+
     try:
-        sentiment = text_nlp.get_sentiment()
+        sentiment = perform_sentiment_analysis(content)
+        sentiment_output = f"Sentiment: neg={sentiment['neg']:.3f}, " \
+                       f"neu={sentiment['neu']:.3f}, " \
+                       f"pos={sentiment['pos']:.3f}, " \
+                       f"compound={sentiment['compound']:.4f}"
+        click.echo(sentiment_output)
     except Exception as e:
         click.echo(f"An error occurred during sentiment analysis: {e}")
         return
-    
-    sentiment_output = f"Sentiment: neg={sentiment['neg']:.3f}, " \
-                   f"neu={sentiment['neu']:.3f}, " \
-                   f"pos={sentiment['pos']:.3f}, " \
-                   f"compound={sentiment['compound']:.4f}"
-    click.echo(sentiment_output)
-
-    if type in ["words", "both"]:
-        for top_count in top:
-            click.echo(f"\nTop {top_count} frequent words:")
-            for word, freq in word_freq.most_common(top_count):
-                click.echo(f"{word}: {freq}")
-
-    if type in ["letters", "both"]:
-        for top_count in top:
-            click.echo(f"\nTop {top_count} frequent letters:")
-            for letter, freq in letter_freq.most_common(top_count):
-                click.echo(f"{letter}: {freq}")
 
     if verbose:
         click.echo("\nVerbose Mode:")
